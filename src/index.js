34aa9f6065cfdac339cfd5ff56b8373a267fe7c3
Gregg Meluski <gmeluski@hotmail.com>
2016-03-04 18:13:21 -0500

    ok, let's push everything into components now

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from "react"
 import ReactDOM from "react-dom"
 import {Provider} from "react-redux"
 import configureStore from "./store"
+import {ReduxRouter} from "redux-react-router"
+import {Route} from "react-router"
+import App from "./containers/App"
+import Home from "./components/Home"
 
 const store = configureStore()
 
 ReactDOM.render(
   <Provider store={store}>
-    <div>haha I work</div>
+    <ReduxRouter>
+      <Route component={App}>
+        <Route path="/" component={Home} />
+      </Route>
+    </ReduxRouter>
   </Provider>,
   document.getElementById("root"))
