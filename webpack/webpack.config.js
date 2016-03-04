c2f5a819f10ff136582c96ed78a72fbf1deb6dc8
Gregg Meluski <gmeluski@hotmail.com>
2016-03-03 19:19:25 -0500

    add the skeleton to webpack this
    just some bare bones things for starters

---
+++ webpack/webpack.config.js
@@ -0,0 +1,37 @@
+var path = require("path")
+var webpack = require("webpack")
+
+module.exports = {
+  context: path.join(__dirname, "..", "src"),
+  devtool: "eval",
+  entry: {
+    index: [
+      "webpack-dev-server/client?http://localhost:3000",
+      "webpack/hot/only-dev-server",
+      "./index.js"
+    ],
+    vendors: ["react", "redux", "react-redux"]
+  },
+  output: {
+    path: path.join(__dirname, "..", "dist"),
+    filename: "[name].js",
+    publicPath: "/static/"
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ["react-hot", "babel"],
+        exclude: /node_modules/,
+        include: path.join("__dirname", "..", "src")
+      },
+    ]
+  },
+  resolve: {
+    extensions: ["", ".react.js", ".js", ".jsx"],
+    modulesDirectories: ["src", "node_modules"]
+  },
+};
