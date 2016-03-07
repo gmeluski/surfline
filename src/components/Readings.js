22ebbb1db6d6c44eb476baac9f5e6b047cea7f33
Gregg Meluski <gmeluski@hotmail.com>
2016-03-06 21:25:43 -0500

    cleaned this out, no more inserting HTML blindly
    just a safer way to do this. also gives way for better security should it be desired later

--- src/components/Readings.js
+++ src/components/Readings.js
@@ -3,11 +3,35 @@ import React, {Component} from "react"
 class Readings extends Component {
   render () {
     if (this.props.isVisible) {
+
+      let safeHtml = this.props.buoyData.split(/<br \/>/)
+        .filter((item) => {
+          if (item.trim()) {
+            return true
+          }
+          return false
+        }).map((item) => {
+          let main = {}
+          let cleaned = item.trim()
+          let titleRegex = /<strong>(.*?)<\/strong>/
+          let description
+
+          main.title = titleRegex.exec(cleaned)[1]
+          description = cleaned.split(/<\/strong>/)
+
+          if (description[1]) {
+            main.description = description[1].trim()
+              .replace(/&#176;/g, "\u00B0")
+          }
+
+          return <div><b>{main.title}</b> {main.description}</div>
+
+        })
       return (
         <div className="row">
           <div className="one column">&nbsp;</div>
-          <div className="eleven columns"
-            dangerouslySetInnerHTML={{__html: this.props.buoyData}}>
+          <div className="eleven columns">
+            {safeHtml}
           </div>
         </div>
       )
